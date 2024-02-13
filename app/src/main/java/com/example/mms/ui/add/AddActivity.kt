21fcc8d67ffd885f20ff327f68f933e9e7c624ed
Nathan Ferry <blackdragon14700@gmail.com>
2024-02-13 13:53:42 +0100

    Adding comment for understanding

--- app/src/main/java/com/example/mms/ui/add/AddActivity.kt
+++ app/src/main/java/com/example/mms/ui/add/AddActivity.kt
@@ -55,6 +55,7 @@ class AddActivity : AppCompatActivity() {
             finish()
         }
 
+        // Calling ScanLoading passing image URI in extra.
         val takePicture: ActivityResultLauncher<Uri?> = registerForActivityResult(
             ActivityResultContracts.TakePicture()
         ) { success ->
