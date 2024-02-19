58933a325f1b49de79691b583c72f15645fc84d8
Nathan Ferry <blackdragon14700@gmail.com>
2024-02-19 14:34:49 +0100

    AAHHHHHAHAAAAAHHAHAHAHHAHHAHAAAHAHAHAAHAHA

--- app/src/main/java/com/example/mms/Utils/OCR.kt
+++ app/src/main/java/com/example/mms/Utils/OCR.kt
@@ -1,24 +1,31 @@
 package com.example.mms.Utils
 
 import android.content.Context
+import android.icu.text.Normalizer2
 import android.os.Parcel
 import android.os.Parcelable
 import android.util.Log
 import com.example.mms.database.inApp.AppDatabase
+import com.example.mms.model.medicines.Medicine
 import com.example.mms.ui.add.ScanLoading
 import com.google.android.gms.tasks.Tasks
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import org.apache.commons.text.similarity.JaroWinklerSimilarity
 import java.net.URI
+import java.text.Normalizer
 
 class OCR(private val db: AppDatabase) {
 
     private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
     private lateinit var result: Text
 
-    //Verify LogCat, Async issues with main thread
+    private fun String.unaccent(): String {
+        return Normalizer2.getNFDInstance().normalize(this).replace(Regex("\\p{Mn}"), "")
+    }
+
     fun recognize(image: InputImage) {
         Tasks.await(
             recognizer.process(image)
@@ -28,8 +35,23 @@ class OCR(private val db: AppDatabase) {
     }
     fun getMedicineInfo(): List<MedicationInfo> {
 
-        result.textBlocks.forEach { block -> Log.d("TextBlock", block.text) }
+        val medicines = db.medicineDao().getAll()
+        val detectedMedicines = mutableListOf<Medicine>()
+        val jws = JaroWinklerSimilarity()
+        Log.d("MedList", medicines.map { it.name }.toString())
 
+        //result.textBlocks.forEach { block -> block.lines.forEach { line -> line.elements.forEach { Log.d("ScanElements", it.text) } } }
+
+        result.textBlocks.forEach { it.lines.forEach { line ->
+            if(line.text.length > 10) {
+                medicines.forEach { med ->
+                    val score = jws.apply(line.text, "${med.name} ${med.type.weight}")
+                    if(score > 0.60) {detectedMedicines.add(med); Log.d("JWS Score", "${line.text} -> ${med.name} ${med.type.weight} : $score")}
+                }
+            }
+        } }
+
+        Log.d("MedicinesList", detectedMedicines.toString())
         return emptyList()
     }
 
