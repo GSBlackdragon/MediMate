b1246e5a8f98ff986274df4743eff1928085031d
Nathan Ferry <blackdragon14700@gmail.com>
2024-02-28 13:48:53 +0100

    mark blank function params and use index accessor

--- app/src/main/java/com/example/mms/ui/createAccount/CAInformationsFragment.kt
+++ app/src/main/java/com/example/mms/ui/createAccount/CAInformationsFragment.kt
@@ -46,7 +46,6 @@ class CAInformationsFragment : Fragment() {
         binding.editBirthdate.setText(viewModel.userData.value?.birthday ?: "----")
         binding.editTaille.setText(viewModel.userData.value?.height?.toString() ?: "")
         binding.editPoids.setText(viewModel.userData.value?.height?.toString() ?: "")
-        //binding.spinnerSexe.setSelection(viewModel.userData.value?.sexe.toString().toInt())
 
         binding.editBirthdate.keyListener = null
         binding.editBirthdate.isFocusable = false
@@ -54,14 +53,14 @@ class CAInformationsFragment : Fragment() {
         binding.editBirthdate.setOnClickListener {
             // init calendar
             val calendar = Calendar.getInstance()
-            val year = calendar.get(Calendar.YEAR)
-            val month = calendar.get(Calendar.MONTH)
-            val day = calendar.get(Calendar.DAY_OF_MONTH)
+            val year = calendar[Calendar.YEAR]
+            val month = calendar[Calendar.MONTH]
+            val day = calendar[Calendar.DAY_OF_MONTH]
 
             // build date picker dialog
             val datePickerDialog = DatePickerDialog(
                 root.context,
-                DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDay ->
+                DatePickerDialog.OnDateSetListener { _, selectedYear, selectedMonth, selectedDay ->
                     val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
                     binding.editBirthdate.setText(selectedDate)
                 },
@@ -77,7 +76,7 @@ class CAInformationsFragment : Fragment() {
 
         // regex for name and surname
         val nameSurnameRegex = Regex("^[a-zA-ZÀ-ÿ-'\\s]+$")
-        val nameSurnameFilter = InputFilter { source, start, end, dest, dstart, dend ->
+        val nameSurnameFilter = InputFilter { source, _, _, _, _, _ ->
             if (source != null && !source.toString().matches(nameSurnameRegex)) {
                 ""  // Si le texte ne correspond pas à la regex, le caractère est supprimé
             } else {
