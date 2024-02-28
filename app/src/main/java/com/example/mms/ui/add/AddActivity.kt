1e6a6cf963174397111606e6cd6619ac715f7461
Nathan Ferry <blackdragon14700@gmail.com>
2024-02-28 12:01:32 +0100

    Refactoring abstract function implementation to lambda expression

--- app/src/main/java/com/example/mms/ui/add/AddActivity.kt
+++ app/src/main/java/com/example/mms/ui/add/AddActivity.kt
@@ -133,8 +133,8 @@ class AddActivity : AppCompatActivity() {
 
         this.searchBar = findViewById(R.id.medi_searchbar)
         this.searchBar.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {/*Empty cause unused*/}
+            override fun afterTextChanged(s: Editable?) {/*Empty cause unused*/}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 updateSearchBar()
