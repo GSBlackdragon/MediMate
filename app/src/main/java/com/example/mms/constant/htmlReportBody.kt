70cd5a476f9a323b6b0f411b0c9a413952afeaee
Romain <romain.gouraud@etu.univ-nantes.fr>
2024-02-28 16:49:13 +0100

    Code cleaning

---
+++ app/src/main/java/com/example/mms/constant/htmlReportBody.kt
@@ -0,0 +1,52 @@
+package com.example.mms.constant
+
+var header = "<body style=\"font-family: Arial, Helvetica, sans-serif\">\n" +
+        "                    <div style=\"text-align: center; margin-bottom: 20px; margin-top: 20px;\">\n" +
+        "                    <div style=\"display: flex; justify-content: space-evenly;align-items: center;margin-top:80px\">\n" +
+        "                        <img src=\"LOGOPATH\" />\n" +
+        "                        <h1 style=\"font-size: 30px; font-weight: bold;\">MEDIMATE</h1>\n" +
+        "                    </div>\n" +
+        "                    <div style=\"display: flex; justify-content: space-evenly;align-items: center;margin-top: 50px;\">\n" +
+        "                        <h2 style=\"font-size: 30px; font-weight: bold;\">Ce document constitue le rapport de prise\n" +
+        "                            médicamenteuse pour le patient M./Mme.PNAME.</h2>\n" +
+        "                    </div>\n" +
+        "                    <div style=\"display: flex; justify-content: flex-start;align-items: flex-start;margin-left: 20px;\">\n" +
+        "                        <h3 style=\"font-size: 20px; font-weight: bold;\">Ce document a été généré le DATEGEN.</h3>\n" +
+        "                    </div>\n" +
+        "                    <h2 style=\"page-break-before: always;\">Rapport de prise des médicaments par le patient par semaine</h2>\n" +
+        "" +
+        "" +
+        "" +
+        "" +
+        "<div style=\"margin:20px\">\n" +"LISTTRAITEMENT" +
+        "</div>\n" +
+
+
+        "TAB"+
+        "    <h2 style=\"page-break-before: always;margin-top:15px\">Confidentialité du document</h2>\n" +
+        "    <p style=\"color:black; font-size: 18px;margin-left:20;margin-right:20;text-align: justify;margin-top:20px \">\n" +
+        "        La préservation de la confidentialité des informations médicales est\n" +
+        "        une préoccupation primordiale dans la rédaction du présent rapport sur la prise de médicaments du patient. Ce document est considéré comme\n" +
+        "        hautement confidentiel et son accès est strictement réservé aux professionnels de la santé impliqués directement\n" +
+        "        dans les soins du patient. Toute divulgation non autorisée de ces informations est expressément interdite,\n" +
+        "        conformément aux lois et réglementations en vigueur relatives à la protection des données médicales.\n" +
+        "        <br><br>Les membres de l'équipe médicale sont tenus de respecter scrupuleusement la confidentialité des\n" +
+        "        détails\n" +
+        "        contenus dans ce rapport, et des mesures de sécurité appropriées ont été mises en place pour garantir la\n" +
+        "        protection des données sensibles. Ces mesures comprennent l'utilisation de systèmes informatiques\n" +
+        "        sécurisés,\n" +
+        "        l'accès restreint aux informations médicales et la formation continue du personnel sur l'importance de\n" +
+        "        maintenir\n" +
+        "        la confidentialité des données.\n" +
+        "        <br><br>La confidentialité de ce rapport vise à instaurer une relation de confiance entre le patient et\n" +
+        "        les\n" +
+        "        professionnels de la santé, favorisant ainsi un environnement propice à des soins de qualité. Tout\n" +
+        "        professionnel\n" +
+        "        de la santé ayant accès à ce document est tenu de respecter les principes éthiques liés à la\n" +
+        "        confidentialité\n" +
+        "        médicale et de prendre les mesures nécessaires pour prévenir tout risque potentiel de divulgation non\n" +
+        "        autorisée.\n" +
+        "    </p>\n" +
+        "</div>\n" +
+        "\n" +
+        "</body>\n"
