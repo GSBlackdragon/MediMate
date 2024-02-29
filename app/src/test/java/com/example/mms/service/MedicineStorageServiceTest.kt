f22a94a55958ce519713d7b50d149993b72df293
Nathan Ferry <blackdragon14700@gmail.com>
2024-02-29 10:34:40 +0100

    Adding test for MedicineStorageService

--- app/src/test/java/com/example/mms/service/MedicineStorageServiceTest.kt
+++ app/src/test/java/com/example/mms/service/MedicineStorageServiceTest.kt
@@ -1,11 +1,21 @@
 package com.example.mms.service
 
+import com.example.mms.database.inApp.AppDatabase
+import com.example.mms.model.MedicineStorage
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.*
 
 class MedicineStorageServiceTest {
+    private val dbMock = mockk<AppDatabase>()
 
     @Test
     fun updateMedicineStorage() {
-
+        every { dbMock.medicineStorageDao().getMedicineStorageByMedicineId(any()) } returns MedicineStorage(1L, 10, 5)
+        val actual = dbMock.medicineStorageDao().getMedicineStorageByMedicineId(1)
+        val expected = MedicineStorage(1L, 10, 5)
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected)
     }
 }
